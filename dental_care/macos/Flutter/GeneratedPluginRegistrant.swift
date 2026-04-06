433796c73cc11cb55d875bfc444e38845f934a2c
Sabstar71 <sabeehwd1786@gmail.com>
2026-04-07 01:29:15 +0500

    fix: Update share_plus to ^8.0.0 for web support - web build now successful

--- dental_care/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dental_care/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_auth
 import firebase_core
 import path_provider_foundation
 import printing
-import share_plus_macos
+import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
